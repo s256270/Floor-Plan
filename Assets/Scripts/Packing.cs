7847f310bd8298338c92bde1c950a901cfea43d8
s256270 <s256270@wakayama-u.ac.jp>
2023-08-11 20:16:51 +0900

    水回りの枠組みと玄関，MBPS

--- Assets/Scripts/Packing.cs
+++ Assets/Scripts/Packing.cs
@@ -6,9 +6,15 @@ public class Packing : CreateRoom
 {
     void Start()
     {
-        Vector3[] range = new Vector3[]{new Vector3(-2050, 1350, 0), new Vector3(2050, 1350, 0), new Vector3(2050, 0, 0), new Vector3(1050, 0, 0), new Vector3(1050, -1350, 0), new Vector3(-2050, -1350, 0)};
+        Vector3[] range = new Vector3[]{new Vector3(-2050, 1850, 0), new Vector3(2050, 1850, 0), new Vector3(2050, -50, 0), new Vector3(1050, -50, 0), new Vector3(1050, -1850, 0), new Vector3(-2050, -1850, 0)};
         createRoom("range", range);
 
+        Vector3[] entrance = new Vector3[]{new Vector3(1050, -50, 0), new Vector3(2050, -50, 0), new Vector3(2050, -1550, 0), new Vector3(1050, -1550, 0)};
+        createRoom("entrance", entrance);
 
+        Vector3[] mbps = new Vector3[]{new Vector3(1050, -1550, 0), new Vector3(2050, -1550, 0), new Vector3(2050, -1850, 0), new Vector3(1050, -1850, 0)};
+        createRoom("mbps", mbps);
+
+        
     }
 }
